53a61412565177133c810441f675a5d4a0bf24fa
Jeff Epler <jepler@gmail.com>
2025-03-24 14:45:03 -0500

    Respond to review comments

--- src/rp2_common/pico_audio_i2s/audio_i2s.c
+++ src/rp2_common/pico_audio_i2s/audio_i2s.c
@@ -62,7 +62,7 @@ const audio_format_t *audio_i2s_setup(const audio_format_t *intended_audio_forma
 
 
     const struct pio_program *program =
-        config->clock_pin_swapped ? &audio_i2s_swapped_program : &audio_i2s_program;
+        config->clock_pins_swapped ? &audio_i2s_swapped_program : &audio_i2s_program;
     uint offset = pio_add_program(audio_pio, program);
 
     audio_i2s_program_init(audio_pio, sm, offset, config->data_pin, config->clock_pin_base);
